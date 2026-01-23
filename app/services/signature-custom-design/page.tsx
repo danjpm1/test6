97415310a33d143c176038ad088173f6aa7db212
danjpm1 <danjpm1@outlook.com>
2026-01-22 22:02:30 -0800

    Update page.tsx

--- app/services/signature-custom-design/page.tsx
+++ app/services/signature-custom-design/page.tsx
@@ -146,7 +146,7 @@ export default function SignatureCustomDesignPage() {
           {/* Row 3: Bottom Video */}
           <div className="flex justify-center">
             <div className="w-[67%] sm:w-[58%] md:w-[50%] lg:w-[47%] relative">
-              <div className="relative w-full" style={{ aspectRatio: '1.96/1' }}>
+              <div className="relative w-full" style={{ aspectRatio: '1.78/1' }}>
                 <video
                   src="/renovation-showcase.mp4"
                   autoPlay
