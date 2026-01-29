f86511f1fd3d51158c9b17d318da0326abe74b62
danjpm1 <danjpm1@outlook.com>
2026-01-28 22:22:07 -0800

    Update page.tsx

--- app/services/commercial-projects/page.tsx
+++ app/services/commercial-projects/page.tsx
@@ -129,7 +129,7 @@ export default function CommercialPage() {
       </section>
 
       {/* 21:9 Image + Text Section - Tesla Style */}
-      <section className="pt-14 pb-20 bg-white">
+      <section className="pt-14 pb-[125px] bg-white">
         <div className="max-w-[1915px] mx-auto px-4 md:px-6">
           <div className="relative w-full aspect-[3/1.045] overflow-hidden rounded-lg mb-11">
             <img
@@ -153,7 +153,7 @@ export default function CommercialPage() {
       </section>
 
       {/* Video Section */}
-      <section className="pt-14 pb-20 bg-white">
+      <section className="pt-[88px] pb-20 bg-white">
         <div className="max-w-[1915px] mx-auto px-4 md:px-6">
           <div className="relative w-full aspect-[3/1.045] overflow-hidden rounded-lg">
             <video
